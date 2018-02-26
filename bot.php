fdbf59f7f4846aef2d5320ffc72b89a976852500
Tinnakorn Peiwnual <tinnakorn.pei@gmail.com>
2018-02-26 18:46:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+				{
 				'type' => 'text',
 				'text' => $text.'/'.$replyToken
+				},
+				{
+				'type' => 'text',
+				'text' => $text.'/////'.$replyToken
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
