8e1525ce67a5cdb9e5fd072dd424b447ed19a294
Tinnakorn Peiwnual <tinnakorn.pei@gmail.com>
2018-02-26 19:34:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,19 +18,11 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				//{            "type":"text",            "text":"Hello, user"        },
-				//{            "type":"text",            "text":"May I help you?"        }  
 				'type' => 'text',
 				'text' => $text.'/'.$replyToken,
 				
 			];
-			$messages .= [
-				//{            "type":"text",            "text":"Hello, user"        },
-				//{            "type":"text",            "text":"May I help you?"        }  
-				'type' => 'text',
-				'text' => $text.'/'.$replyToken,
-				
-			];
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
